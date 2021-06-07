aad65b09404545a37c565cf65d03216d333d6376
Steven Vaughn <steve.h.vaughn@gmail.com>
2021-06-07 13:12:39 -0600

    completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(num){
+  if (num <= 400) {
+    return "This one is on me!";
+  } else if (num > 2000 && num < 2500) {
+    return "I will gladly take your thirty bucks.";
+  } else if (num > 2500) {
+    return "No can do.";
+  }
+};
+
+function ternaryCheckCity(city){
+  return (city == "NYC") ? "Ok, sounds good." : "No go.";
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let charm;
+  switch(tip) {
+    case "generous":
+    charm = "Thank you so much.";
+    break;
+    case "not as generous":
+    charm = "Thank you.";
+    break;
+    default:
+    charm = "Bye."  
+  }
+  return charm;
 }
